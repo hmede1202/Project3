7382acae91c0fa9373caf8dc3a22b2102652bae2
hmede1202 <77769174+hmede1202@users.noreply.github.com>
2021-04-13 00:43:41 -0700

    trying to fix the map

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 function int(){
   alert('it works');
   
-var map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 48.85853948884557, lng: 2.294497391554784 },
-    zoom: 8,
+  var el = document.getElementById('canvas');
+  var myLocation = new google.maps.LatLng(48.85853948884557, 2.294497391554784);
+  var mapOptions = {
+    center: myLocation,
+    zoom: 18,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.BOTTOM_CETER
+    }
+  };
+ 
+  var myMap = new google.maps.Map(el, mapOptions);
   });
+
 }
 
-google.maps.event.addDomListenser(window, 'load', init);
+google.maps.event.addDomListener(window, 'load', init);
 
 , 
