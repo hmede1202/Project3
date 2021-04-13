204a3fb8f5b1a2b155a4be0e08cf6fcf870948d4
hmede1202 <77769174+hmede1202@users.noreply.github.com>
2021-04-12 22:25:39 -0700

    Added script

--- js/script.js
+++ js/script.js
@@ -1,38 +1,6 @@
 function int(){
   alert('it works')
   
-  var el = document.getElementById('canvas');
-  var myLocation = new google.maps.LatLng(33.51713103580776, -117.76123910392413);
-  var mapOptions = {
-    center: myLocation,
-    zoom:18,
-    mapTypeId: google.maps.MapTypeId.SATELLITE,
-    mapTypeControlOptions:{
-      position: google.maps.ControlPosition.BOTTOM_CENTER
-      }  
-    };
-
-  var myMap = new google.maps.Map(el, mapOptions);
-
-  var marker = new google.maps.Marker({
-    position: myLocation,
-    map: myMap,
-    animation: google.maps.Animation.BOUNCE,
-    icon: 'iit-icon.png'  
-  });  
-  
-  var contentString = '<h1>Hunter Joseph Mede</h1><Summer is almost here!!!</p>'
-  
-  var inforwindow = new google.maps.InfoWindow ({
-    content: contentString  
-  });
-  
-  google.maps.event.addListener(marker, 'mouseover', function() {
-    infowindow.open(myMap,marker);
-  });
-  
-}
-
 let map;
 
 function initMap() {
@@ -41,3 +9,5 @@ function initMap() {
     zoom: 8,
   });
 }
+
+window.addEventListener('load', init);
