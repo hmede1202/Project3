66eee7e87bb65e5708d1aad83d22194c75b63875
hmede1202 <77769174+hmede1202@users.noreply.github.com>
2021-04-12 23:02:01 -0700

    changing lat and longitude for eiffel tower

--- js/script.js
+++ js/script.js
@@ -5,9 +5,11 @@ var map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 33.51713103580776, lng: -117.76123910392413 },
+    center: { lat: 48.85853948884557, lng: 2.294497391554784 },
     zoom: 8,
   });
 }
 
 window.addEventListener('load', init);
+
+, 
