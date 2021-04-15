3c50aac3bb0f94ddf1782199ad22d994b745f415
hmede1202 <77769174+hmede1202@users.noreply.github.com>
2021-04-15 15:16:45 -0700

    Trying to fix bx slider

--- js/script.js
+++ js/script.js
@@ -32,43 +32,8 @@ function init(){
     infowindow.open(myMap, marker);
   });
   
- $(function(){
- 
-   var width = 720;
-   var animationSpeed = 1000;
-   var pause 2500:
-   var currentSlide = 1
-   
-   var $slider = $('#slider');
-   var $slideContainer = $slider.find('.slides');
-   var $slides = $slideContainer.find('.slide');
-   
-   var interval;
-   
-   function startSlider(){
-     interval = setInterval(function() {
-       $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function ()
-         currentSlide++;
-         if (currentSlide === $slides.length){
-           currnetSlide = 1;
-           $slideContainer.css(margin-left', 0);
-         }
-       });
-     }, pause);
-     
-   }
-   
-   function stopSlider() {
-     clearInterval(interval):
-   }
-
-   
-   $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
-
-startSlider();
-  
-});
-
+	
 }
+	
 google.maps.event.addDomListener(window, 'load', init);
 
